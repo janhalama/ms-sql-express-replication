4777970d15212365ffb3c5d5c369044c01743269
Jan Halama <halama@jablocom.com>
2016-01-10 14:32:51 +0100

    Added new snapshot replication test ReplicationTestTableWithForeignKeyRepeate

--- source/Replication.SqlClient.IntegrationTest/TableSnapshotReplicationStrategyTest.cs
+++ source/Replication.SqlClient.IntegrationTest/TableSnapshotReplicationStrategyTest.cs
@@ -46,13 +46,14 @@ namespace Jh.Data.Sql.Replication.SqlClient.IntegrationTest
                     {
                         var standard = new Standard() { StandardName = "Standard" };
                         testContext.Standards.Add(standard);
-                        student = testContext.Students.Add(new Student() {
+                        student = testContext.Students.Add(new Student()
+                        {
                             Standard = standard,
                             Weight = 70,
                             Height = 178,
                             StudentName = "Jan Halama",
                             DateOfBirth = DateTime.Now,
-                            Photo = new byte[] { 0,1,2,3}
+                            Photo = new byte[] { 0, 1, 2, 3 }
                         });
                         testContext.SaveChanges();
                     }
@@ -67,9 +68,9 @@ namespace Jh.Data.Sql.Replication.SqlClient.IntegrationTest
                     replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Students", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
                     using (var testContext = new TestContext(_connectionString + $";Database={TARGET_DATABASE_NAME}"))
                     {
-                        Assert.True(testContext.Students.Any(s => s.StudentId == student.StudentId && s.StudentName == s.StudentName && s.Standard.StandardId == student.Standard.StandardId),"Target db not as expected after replication");
+                        Assert.True(testContext.Students.Any(s => s.StudentId == student.StudentId && s.StudentName == s.StudentName && s.Standard.StandardId == student.Standard.StandardId), "Target db not as expected after replication");
                     }
-                 }
+                }
                 finally
                 {
                     _testDatabaseProvider.DropTestDatabase(TARGET_DATABASE_NAME);
@@ -98,8 +99,8 @@ namespace Jh.Data.Sql.Replication.SqlClient.IntegrationTest
                         standard = testContext.Standards.Add(standard);
                         testContext.SaveChanges();
                         List<Student> students2Add = new List<Student>();
-                        for(int i = 0; i < 100000; i++)
-                        { 
+                        for (int i = 0; i < 100000; i++)
+                        {
                             students2Add.Add(new Student()
                             {
                                 StandardId = standard.StandardId,
@@ -139,12 +140,67 @@ namespace Jh.Data.Sql.Replication.SqlClient.IntegrationTest
                     replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Students", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
                     using (var testContext = new TestContext(_connectionString + $";Database={TARGET_DATABASE_NAME}"))
                     {
-                        Assert.Equal(100000,testContext.Students.Count());
+                        Assert.Equal(100000, testContext.Students.Count());
                         for (int i = 0; i < 100000; i += 99)
                         {
                             string expectedStudentName = $"Student {i}";
                             Assert.True(null != testContext.Students.Single(s => s.StudentName == expectedStudentName && s.Weight == 70 + i && s.Height == 178 + i), $"Student {i} not found");
                         }
+                    }
+                }
+                finally
+                {
+                    _testDatabaseProvider.DropTestDatabase(TARGET_DATABASE_NAME);
+                }
+            }
+            finally
+            {
+                _testDatabaseProvider.DropTestDatabase(SOURCE_DATABASE_NAME);
+            }
+        }
+        [Fact]
+        public void ReplicationTestTableWithForeignKeyRepeate()
+        {
+            string SOURCE_DATABASE_NAME = _testDatabaseProvider.GenerateUniqueDatabaseName("TestReplication_Source");
+            string TARGET_DATABASE_NAME = _testDatabaseProvider.GenerateUniqueDatabaseName("TestReplication_Target");
+            const string SCHEMA = "dbo";
+            _testDatabaseProvider.CreateTestDatabase(SOURCE_DATABASE_NAME);
+            try
+            {
+                _testDatabaseProvider.CreateTestDatabase(TARGET_DATABASE_NAME);
+                try
+                {
+                    Student student;
+                    using (var testContext = new TestContext(_connectionString + $";Database={SOURCE_DATABASE_NAME}"))
+                    {
+                        var standard = new Standard() { StandardName = "Standard" };
+                        testContext.Standards.Add(standard);
+                        student = testContext.Students.Add(new Student()
+                        {
+                            Standard = standard,
+                            Weight = 70,
+                            Height = 178,
+                            StudentName = "Jan Halama",
+                            DateOfBirth = DateTime.Now,
+                            Photo = new byte[] { 0, 1, 2, 3 }
+                        });
+                        testContext.SaveChanges();
+                    }
+                    using (var testContext = new TestContext(_connectionString + $";Database={TARGET_DATABASE_NAME}"))
+                    {
+                        testContext.Standards.Select(s => s.StandardId > 0);
+                        testContext.Students.Select(s => s.StudentId > 0);
+                        testContext.SaveChanges();
+                    }
+                    IReplicationStrategy replicationStrategy = new Strategies.TableSnapshotReplicationStrategy(_connectionString, _connectionString, _logMock.Object);
+                    replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Standards", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
+                    replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Students", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
+                    //repeate; test that foreign keys can be used in tables that are replicated using replication strategy
+                    replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Standards", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
+                    replicationStrategy.Replicate(new ReplicationArticle() { SourceDatabaseName = SOURCE_DATABASE_NAME, TargetDatabaseName = TARGET_DATABASE_NAME, ArticleName = "Students", SourceSchema = SCHEMA, TargetSchema = SCHEMA, ArticleType = DataContracts.Enums.eArticleType.TABLE });
+                    using (var testContext = new TestContext(_connectionString + $";Database={TARGET_DATABASE_NAME}"))
+                    {
+                        Assert.True(testContext.Students.Any(s => s.StudentId == student.StudentId && s.StudentName == s.StudentName && s.Standard.StandardId == student.Standard.StandardId), "Target db not as expected after replication");
                     }
                 }
                 finally
