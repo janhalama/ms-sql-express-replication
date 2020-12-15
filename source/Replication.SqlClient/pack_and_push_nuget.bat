9eb5461e49a3073e5702dd816bfa4e687f3072e9
Jan Halama <jenik.halama@gmail.com>
2020-12-15 23:41:49 +0100

    chore: nuget publish script added

--- source/Replication.SqlClient/pack_and_push_nuget.bat
+++ source/Replication.SqlClient/pack_and_push_nuget.bat
@@ -1,6 +1,5 @@
-SET ver=1.5.0
+SET ver=1.7.0
 SET projectName=Jh.Data.Sql.Replication.SqlClient
-
 nuget pack %projectName%.csproj -Build -Symbols -IncludeReferencedProjects
-nuget push %projectName%.%ver%.nupkg
-nuget push %projectName%.%ver%.symbols.nupkg
+nuget push %projectName%.%ver%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey oy2mtk3rryuhebwnud3qednf66wtn4eerjbrpjdrlg7ghy
+nuget push %projectName%.%ver%.symbols.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey oy2mtk3rryuhebwnud3qednf66wtn4eerjbrpjdrlg7ghy
