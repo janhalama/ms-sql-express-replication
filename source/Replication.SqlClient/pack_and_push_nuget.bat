9a0dd26b29e90c8ddb4c216f86ee8bdba0c877dd
Jan Halama <jenik.halama@gmail.com>
2020-12-15 23:44:02 +0100

    chore: new assembly version

--- source/Replication.SqlClient/pack_and_push_nuget.bat
+++ source/Replication.SqlClient/pack_and_push_nuget.bat
@@ -1,4 +1,4 @@
-SET ver=1.7.0
+SET ver=1.8.0
 SET projectName=Jh.Data.Sql.Replication.SqlClient
 nuget pack %projectName%.csproj -Build -Symbols -IncludeReferencedProjects
 nuget push %projectName%.%ver%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey oy2mtk3rryuhebwnud3qednf66wtn4eerjbrpjdrlg7ghy
