2b4c9559406f6c1cb5ddd53257d86d799b69cf23
Jan Halama <jenik.halama@gmail.com>
2020-12-15 23:42:27 +0100

    feat: add catalog analyzer db tool

---
+++ source/Replication.SqlClient/DbTools/CatalogAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jh.Data.Sql.Replication.SqlClient.DbTools
+{
+    class Class1
+    {
+    }
+}
---
+++ source/Replication.SqlClient/DbTools/Interfaces/ICatalogAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using Jh.Data.Sql.Replication.SqlClient.DbTools.DataContracts;
+
+namespace Jh.Data.Sql.Replication.SqlClient.DbTools.Interfaces
+{
+    /// <summary>
+    /// Analyzes the database table
+    /// </summary>
+    public interface ITableSchemaAnalyzer
+    {
+        /// <summary>
+        /// Gets the table columns.
+        /// </summary>
+        /// <param name="catalog">The catalog.</param>
+        /// <param name="schema">The schema.</param>
+        /// <param name="table">The table.</param>
+        /// <returns></returns>
+        Column[] GetTableColumns(string catalog, string schema, string table);
+        /// <summary>
+        /// Gets the table information.
+        /// </summary>
+        /// <param name="catalog">The catalog.</param>
+        /// <param name="schema">The schema.</param>
+        /// <param name="table">The table.</param>
+        /// <returns></returns>
+        Table GetTableInfo(string catalog, string schema, string table);
+    }
+}
