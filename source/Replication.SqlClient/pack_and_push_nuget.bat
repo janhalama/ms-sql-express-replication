ab322c6d818623a29caf670b6d2b5280f208c6fb
Jan Halama <jenik.halama@gmail.com>
2020-12-18 00:01:04 +0100

    chore: remove Nuget api key from Git repository

--- source/Replication.SqlClient/pack_and_push_nuget.bat
+++ source/Replication.SqlClient/pack_and_push_nuget.bat
@@ -1,5 +1,6 @@
 SET ver=1.8.0
 SET projectName=Jh.Data.Sql.Replication.SqlClient
+SET apiKey=%1
 nuget pack %projectName%.csproj -Build -Symbols -IncludeReferencedProjects
-nuget push %projectName%.%ver%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey oy2mtk3rryuhebwnud3qednf66wtn4eerjbrpjdrlg7ghy
-nuget push %projectName%.%ver%.symbols.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey oy2mtk3rryuhebwnud3qednf66wtn4eerjbrpjdrlg7ghy
+nuget push %projectName%.%ver%.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey %apiKey%
+nuget push %projectName%.%ver%.symbols.nupkg -Source https://api.nuget.org/v3/index.json -ApiKey %apiKey%
